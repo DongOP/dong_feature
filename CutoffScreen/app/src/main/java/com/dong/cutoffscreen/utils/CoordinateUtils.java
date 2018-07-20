a87257806c99991ba794fc6734956de137879bd9
DongOP <donking0030@gmail.com>
2018-07-20 21:00:33 +0800

    add coordinate transformation

--- CutoffScreen/app/src/main/java/com/dong/cutoffscreen/utils/CoordinateUtils.java
+++ CutoffScreen/app/src/main/java/com/dong/cutoffscreen/utils/CoordinateUtils.java
@@ -40,8 +40,8 @@ public class CoordinateUtils {
         float densityTarget = DeviceInfoUtils.getDensity(context);
         // 存放转换后的坐标
         Map<String, Integer> targetMap = new HashMap<>();
-        int xProportion = 0;
-        int yProportion = 0;
+        float xProportion = 0;
+        float yProportion = 0;
         // 解析数据 start
         try {
             JSONObject jsonObject = new JSONObject(msg);
@@ -61,8 +61,8 @@ public class CoordinateUtils {
                         y = json.optInt(JsonDataUtils.Y_POINT);
                     }
                 }
-                xProportion = xTargetImageRes / xImageRes;
-                yProportion = yTargetImageRes / yImageRes;
+                xProportion = (float)xTargetImageRes / xImageRes;
+                yProportion = (float)yTargetImageRes / yImageRes;
                 LogUtils.logd(TAG, "changeXY, begin_time=" + begin_time + ", end_time=" + end_time + ", begin_x=" + x + ", begin_y=" + y + ", density=" + density + ", xImageRes=" + xImageRes + ", yImageRes=" + yImageRes +
                         ", xTargetImageRes=" + xTargetImageRes + ", yTargetImageRes=" + yTargetImageRes);
             }
@@ -72,7 +72,7 @@ public class CoordinateUtils {
         }
         // 解析数据 end
         // 密度是大于0，context不能为空。否则返回
-        if (density <= 0 || null == context || x < 0 || y < 0) {
+        if (density <= 0 || null == context || x < 0 || y < 0 || xProportion == 0) {
             LogUtils.loge(TAG, "数据错误，需检查数据来源。");
             return null;
         }
@@ -91,7 +91,7 @@ public class CoordinateUtils {
             } else {
                 // x轴相同y轴不同,换算公式：y3 = y * (y2 / y1)
 //                targetMap.clear();
-                int yTarget = y * yProportion;
+                int yTarget = (int)(y * yProportion);
                 LogUtils.logd(TAG, "操作机与执行机 x轴相同、y轴不同=" + yProportion);
                 targetMap.put(X_TARGET, x);
                 targetMap.put(Y_TARGET, yTarget);
@@ -101,7 +101,7 @@ public class CoordinateUtils {
             // x轴不同，y轴相同,换算公式：x3 = x * (x2 / x1)
             if (y == yTargetImageRes) {
 //                targetMap.clear();
-                int desX = x * xProportion;
+                int desX = (int)(x * xProportion);
                 LogUtils.logd(TAG, "操作机与执行机 y轴相同,x轴不同" + xProportion);
                 targetMap.put(X_TARGET, desX);
                 targetMap.put(Y_TARGET, y);
@@ -109,8 +109,8 @@ public class CoordinateUtils {
             } else {
 //                targetMap.clear();
                 // x轴不同，y轴不同.换算公式：x3 = x * (x2 / x1)、y3 = y * (y2 / y1)
-                int desX = x * xProportion;
-                int desY = y * yProportion;
+                int desX = (int)(x * xProportion);
+                int desY = (int)(y * yProportion);
                 LogUtils.logd(TAG, "操作机与执行机 x y轴不同，分辨率比=" + xProportion + "， y的比例=" + yProportion);
                 targetMap.put(X_TARGET, desX);
                 targetMap.put(Y_TARGET, desY);
