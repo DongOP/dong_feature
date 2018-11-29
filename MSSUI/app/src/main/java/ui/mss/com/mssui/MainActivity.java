d06d6c83320f408695e246f6791fd18296baf154
DongOP <donking0030@gmail.com>
2018-11-29 15:09:42 +0800

    add 乒乓功能

--- MSSUI/app/src/main/java/ui/mss/com/mssui/MainActivity.java
+++ MSSUI/app/src/main/java/ui/mss/com/mssui/MainActivity.java
@@ -5,15 +5,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 
 import ui.mss.com.mssui.view.SettingDialog;
 
 public class MainActivity extends Activity {
 
-    private Button mStartDialog;
     private Context mContext;
+    private Button mStartDialog;
+    private Button mPingPangBtn;
+    // 乒乓键的状态
+    private Boolean isTrue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +26,10 @@ public class MainActivity extends Activity {
 
     private void initView() {
         mContext = MainActivity.this;
+        // 默认乒乓键第一次为false
+        isTrue = false;
         mStartDialog = (Button) findViewById(R.id.btn_dialog);
+        mPingPangBtn = (Button) findViewById(R.id.btn_pingpang);
 
         mStartDialog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,11 +37,35 @@ public class MainActivity extends Activity {
                 startDialog();
             }
         });
+
+        // 需要先初始化按钮的状态
+        checkBtnStatus();
+        mPingPangBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isTrue) {
+                    mPingPangBtn.setText("乒乓键（打开）");
+                    isTrue = false;
+                } else {
+                    mPingPangBtn.setText("乒乓键（关闭）");
+                    isTrue = true;
+                }
+            }
+        });
+    }
+
+    private void checkBtnStatus() {
+        if (isTrue) {
+            mPingPangBtn.setText("乒乓键（打开）");
+            isTrue = false;
+        } else {
+            mPingPangBtn.setText("乒乓键（关闭）");
+            isTrue = true;
+        }
     }
 
     private void startDialog() {
         SettingDialog settingDialog = new SettingDialog(mContext);
-//        settingDialog.setCancelable(false);
         settingDialog.show();
     }
 }
