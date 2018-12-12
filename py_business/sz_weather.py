f6cbf984deaf8ca9465bf2200118588928adde8d
DongOP <donking0030@gmail.com>
2018-12-12 22:41:54 +0800

    add 间隔30s获取一次天气信息

--- py_business/sz_weather.py
+++ py_business/sz_weather.py
@@ -1,62 +1,62 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import time
 
 # resp=urlopen('http://www.weather.com.cn/weather/101280601.shtml')
 
+def getWebMessage():
+	resp = urlopen('http://www.weather.com.cn/weather/101280601.shtml')
+	return resp
+
 def getTodayWeather():
-	resp=urlopen('http://www.weather.com.cn/weather/101280601.shtml')
-	soup=BeautifulSoup(resp,'html.parser')
-	tagDate=soup.find('ul', class_="t clearfix")
-	dates=tagDate.h1.string
+	resp = getWebMessage()
+	soup = BeautifulSoup(resp,'html.parser')
+	tagDate = soup.find('ul', class_="t clearfix")
+	dates = tagDate.h1.string
 
-	tagToday=soup.find('p', class_="tem")
+	tagToday = soup.find('p', class_="tem")
 	try:
-		temperatureHigh=tagToday.span.string
+		temperatureHigh = tagToday.span.string
 	except AttributeError as e:
-		temperatureHigh=tagToday.find_next('p', class_="tem").span.string
+		temperatureHigh = tagToday.find_next('p', class_="tem").span.string
 
-	temperatureLow=tagToday.i.string
-	weather=soup.find('p', class_="wea").string
+	temperatureLow = tagToday.i.string
+	weather = soup.find('p', class_="wea").string
 
-	tagWind=soup.find('p',class_="win")
-	winL=tagWind.i.string
+	tagWind = soup.find('p',class_="win")
+	winL = tagWind.i.string
 
-	print('日期：'+dates)
-	# print('风级：'+winL)
-	# print('最低温度：'+temperatureLow)
-	# print('最高温度：'+temperatureHigh)
-	# print('天气：'+weather)
-	print('风级：'+winL + ', 最低温度：'+temperatureLow + ', 最高温度：'+temperatureHigh + ', 天气：'+weather)
-	print('\n')
+	print('日期：' + dates)
+	print('风级：' + winL + ', 最低温度：' + temperatureLow + ', 最高温度：' + temperatureHigh + ', 天气：' + weather + '\n')
 
 def getTomorrowWeather():
-	resp=urlopen('http://www.weather.com.cn/weather/101280601.shtml')
-	soup=BeautifulSoup(resp,'html.parser')
-	tagDate=soup.find('ul', class_="t clearfix")
-	dayDates=tagDate.find('li', class_="sky skyid lv2")
+	resp = getWebMessage()
+	soup = BeautifulSoup(resp,'html.parser')
+	tagDate = soup.find('ul', class_="t clearfix")
+	dayDates = tagDate.find('li', class_="sky skyid lv2")
 	dates = dayDates.h1.string
-	tagToday=dayDates.find('p', class_="tem")
+	tagTomorrow = dayDates.find('p', class_="tem")
 	try:
-		temperatureHigh=tagToday.span.string
+		temperatureHigh = tagTomorrow.span.string
 	except AttributeError as e:
-		temperatureHigh=tagToday.find_next('p', class_="tem").span.string
+		temperatureHigh = tagTomorrow.find_next('p', class_="tem").span.string
 
-	temperatureLow=tagToday.i.string
-	weather=dayDates.find('p', class_="wea").string
+	temperatureLow = tagTomorrow.i.string
+	weather = dayDates.find('p', class_="wea").string
 
-	tagWind=dayDates.find('p',class_="win")
-	winL=tagWind.i.string
+	tagWind = dayDates.find('p',class_="win")
+	winL = tagWind.i.string
 
-	print('日期：'+dates)
-	print('风级：'+winL + ', 最低温度：'+temperatureLow + ', 最高温度：'+temperatureHigh + ', 天气：'+weather)
-	# print('风级：'+winL)
-	# print('最低温度：'+temperatureLow)
-	# print('最高温度：'+temperatureHigh)
-	# print('天气：'+weather)
+	print('日期：' + dates)
+	print('风级：' + winL + ', 最低温度：'+temperatureLow + ', 最高温度：'+temperatureHigh + ', 天气：'+weather)
+	print('\n' + '---------------------------我是分割线------------------------' + '\n')
 
 def main_loop():
-	getTodayWeather()
-	getTomorrowWeather()
+
+	while True:
+		getTodayWeather()
+		getTomorrowWeather()
+		time.sleep(30)
 
 main_loop()
